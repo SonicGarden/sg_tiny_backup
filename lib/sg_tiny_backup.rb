159eb36e40aa4a09e65a265504a247c4ca1308f1
Shunichi Ikegami <sike.tm@gmail.com>
2022-08-17 22:08:15 +0900

    bundle gem

---
+++ lib/sg_tiny_backup.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "sg_tiny_backup/version"
+
+module SgTinyBackup
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/sg_tiny_backup_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe SgTinyBackup do
+  it "has a version number" do
+    expect(SgTinyBackup::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
